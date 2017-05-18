9c42dff7acd57126b617170e4b9991e4be613492
Justin Reich <justinreich.dev@gmail.com>
2017-05-18 15:39:20 -0500

    Initial configuration of routes file

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,6 +11,60 @@
 |
 */
 
+// other pages
+
 Route::get('/', function () {
-    return view('welcome');
+    return view('pages.welcome');
 });
+
+Route::get('/about', function () {
+    return view('pages.about');
+});
+
+
+// posts pages
+
+Route::get('/posts', function () {
+    return view('posts.index');
+});
+
+Route::get('/posts/create', function () {
+    return view('posts.create');
+});
+
+Route::get('/posts/{id}', function ($id) {
+    return view('posts.show');
+});
+
+Route::get('/posts/{id}/edit', function ($id) {
+    return view('posts.edit');
+});
+
+
+
+// users pages
+
+Route::get('/users', function () {
+    return view('users.index');
+});
+
+Route::get('/users/{id}/edit', function ($id) {
+    return view('users.edit');
+});
+
+Route::get('/users/{id}', function ($id) {
+    return view('users.show');
+});
+
+
+// auth pages
+
+Route::get('/auth/login', function () {
+    return view('auth.login');
+});
+
+Route::get('/auth/register', function () {
+    return view('auth.register');
+});
+
+
