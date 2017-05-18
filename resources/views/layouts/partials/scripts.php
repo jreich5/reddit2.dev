98f5186166330ccab687d00395f01fa3ff430d43
Justin Reich <justinreich.dev@gmail.com>
2017-05-18 15:58:44 -0500

    Partials

---
+++ resources/views/layouts/partials/scripts.php
@@ -0,0 +1,7 @@
+<!-- jQuery Version 2.2.4 -->
+<script src="http://code.jquery.com/jquery-2.2.4.min.js"></script>
+
+<!-- Bootstrap Core JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
+
+@yield('customScript')
