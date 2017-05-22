80e730293a821ec59df4ac61dcf8537018ca56a2
Justin Reich <justinreich.dev@gmail.com>
2017-05-22 16:57:34 -0500

    Completed seeders for all tables

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -14,8 +14,10 @@ class DatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call(UserTableSeeder::class);
-
+        $this->call(UserTableSeeder::class);
+        $this->call(PostTableSeeder::class);
+        $this->call(VoteTableSeeder::class);
+        
         Model::reguard();
     }
 }
