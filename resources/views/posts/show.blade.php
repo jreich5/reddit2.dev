2dc29a8e34925fc5e27a89290827488ace37a84b
Justin Reich <justinreich.dev@gmail.com>
2017-05-30 09:05:00 -0500

    Finished views for post show and index

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,27 +1,36 @@
 @extends('layouts.master')
 
 @section('title')
-	<title>Reddit2: </title>
+    <title>Reddit2: </title>
 @stop
 
 @section('content')
-	
+    <article>
+        <h1 class="text-center">{{ $post->title }} <small> By {{ $post->user->name }}</small></h1>
+        <img class="center-block"  src="{{ $post->image }}" width="100%">
+        <p>{{ $post->content }}</p>
+        <a class="btn btn-primary" href="{{ action('PostsController@edit', $post->id) }}">Edit Post</a>
+    </article>
 @stop
 
 @section('pageCSS')
 
-	<style type="text/css">
-		
+    <style type="text/css">
 
-	</style>
+        img {
+            border: 2px solid black;
+            margin: 16px;
+        }
+
+    </style>
 
 @stop
 
 @section('pageJS')
 
-	<script type="text/javascript">
-		
-		
-	</script>
+    <script type="text/javascript">
+        
+        
+    </script>
 
 @stop
