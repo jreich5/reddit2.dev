50b550d59ec488caa05083312441d050bd4cf7bd
Justin Reich <justinreich.dev@gmail.com>
2017-05-31 11:38:19 -0500

    Completed post create view

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,36 +1,66 @@
 @extends('layouts.master')
 
 @section('title')
-	<title>Reddit2: </title>
+    <title>Reddit2: Create Post</title>
 @stop
 
 @section('content')
-	<h1>Create Post</h1>
-	{!! Form::open(array('url' => 'posts/create')) !!}
-	
-		{!! Form::label('email', 'E-Mail Address', array('class' => '')) !!}
-		{!! Form::text('email') !!}
-		{!! Form::label('password', 'Password', array('class' => '')) !!}
-		{!! Form::password('password') !!}
-		{!! Form::submit('Submit') !!}
 
-	{!! Form::close() !!}
+    <h1 class="text-center">Create Post</h1>
+
+
+    @if (count($errors) > 0)
+        <div class="alert alert-danger">
+            <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+            </ul>
+        </div>
+    @endif
+
+    {!! Form::open(array('url' => 'posts')) !!}
+    
+        <div class="form-group">
+            {!! Form::label('title', 'Post Title') !!}
+            {!! Form::text('title', null, array('class' => 'form-control')) !!}
+        </div>
+
+        <div class="form-group">
+            {!! Form::label('url', 'Post Url') !!}
+            {!! Form::text('url', null, array('class' => 'form-control')) !!}
+        </div>    
+
+        <div class="form-group">
+            {!! Form::label('image', 'Post Image Url') !!}
+            {!! Form::text('image', null, array('class' => 'form-control')) !!}
+        </div>    
+
+        <div class="form-group">
+            {!! Form::label('content', 'Post Content') !!}
+            {!! Form::textarea('content', null, array('class' => 'form-control')) !!}
+        </div>      
+
+        {!! Form::submit('Submit', array('class' => 'btn btn-default')) !!}
+
+    {!! Form::close() !!}
+
 @stop
 
 @section('pageCSS')
 
-	<style type="text/css">
-		
+    <style type="text/css">
+        
 
-	</style>
+    </style>
 
 @stop
 
 @section('pageJS')
 
-	<script type="text/javascript">
-		
-		
-	</script>
+    <script type="text/javascript">
+        
+        
+    </script>
 
 @stop
