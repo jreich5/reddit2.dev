5d88893468719ded1d5b18150e1339b78fdedc56
Justin Reich <justinreich.dev@gmail.com>
2017-05-31 11:36:45 -0500

    Completed logic for index and show methods

--- app/Http/Controllers/UsersController.php
+++ app/Http/Controllers/UsersController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
+use App\User;
 use App\Http\Controllers\Controller;
 
 class UsersController extends Controller
@@ -16,7 +17,8 @@ class UsersController extends Controller
      */
     public function index()
     {
-        //
+        $users = User::paginate(10);
+        return view('users.index', compact('users'));
     }
 
     /**
@@ -48,7 +50,9 @@ class UsersController extends Controller
      */
     public function show($id)
     {
-        //
+        $user = User::findOrFail($id);
+        $userPosts = $user->posts;
+        return view("users.show", compact('user', 'userPosts'));
     }
 
     /**
@@ -59,7 +63,8 @@ class UsersController extends Controller
      */
     public function edit($id)
     {
-        //
+        $user = User::findOrFail($id);
+        return view("users.edit", compact("user"));
     }
 
     /**
@@ -71,7 +76,13 @@ class UsersController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $user = user::findOrFail($id);
+        $user->name = $request->name;
+        $user->email = $request->email;
+        $user->password = $request->password;
+        $user->save();
+
+        return redirect()->action('UsersController@show', $user->id);
     }
 
     /**
